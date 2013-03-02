5f82dedb7fb03b505ffc6c72c1087e49f9bf5178
PoliM <marco.a.poli@gmail.com>
2013-03-02 19:26:01 +0100

    Initial elements and layout

--- src/ch/ocram/tcptap/MainViewController.scala
+++ src/ch/ocram/tcptap/MainViewController.scala
@@ -12,17 +12,26 @@ import scalafx.scene.control.TextField
 class MainViewController extends jfxf.Initializable {
 
   @jfxf.FXML
-  private var txtAddress: jfxsc.TextField = null
-  private var address: TextField = _
+  private var txtListenPort: jfxsc.TextField = null
+  private var listenPort: TextField = _
 
+  @jfxf.FXML
+  private var txtTargetHost: jfxsc.TextField = null
+  private var targetHost: TextField = _
+
+  @jfxf.FXML
+  private var txtTargetPort: jfxsc.TextField = null
+  private var targetPort: TextField = _
 
   @jfxf.FXML
   private def onStartMonitoring(event: jfxe.ActionEvent) {
-    println(address.text);
+    println(s"${listenPort.text.value} ==> ${targetHost.text.value}:${targetPort.text.value}");
   }
 
 
   def initialize(url: URL, rb: util.ResourceBundle) {
-    address = new TextField(txtAddress)
+    listenPort = new TextField(txtListenPort)
+    targetHost = new TextField(txtTargetHost)
+    targetPort = new TextField(txtTargetPort)
   }
 }
